58673579f1e9d730d637cc15967785ff10ee49eb
smysergio <smysergio@gmail.com>
2018-10-24 19:33:57 +0100

    leds

--- my_leds.py
+++ my_leds.py
@@ -10,15 +10,15 @@ led_green = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(led_red,GPIO.OUT)
-GPIO.setup(led_yellow,GPIO.OUT)
+#GPIO.setup(led_red,GPIO.OUT)
+#PIO.setup(led_yellow,GPIO.OUT)
 GPIO.setup(led_green,GPIO.OUT)
 print "LED on"
-GPIO.output(led_red,GPIO.HIGH)
-GPIO.output(led_yellow,GPIO.HIGH)
+#GPIO.output(led_red,GPIO.HIGH)
+#GPIO.output(led_yellow,GPIO.HIGH)
 GPIO.output(led_green,GPIO.HIGH)
 time.sleep(1)
 print "LED off"
-GPIO.output(led_red,GPIO.LOW)
-GPIO.output(led_yellow,GPIO.LOW)
+#GPIO.output(led_red,GPIO.LOW)
+#GPIO.output(led_yellow,GPIO.LOW)
 GPIO.output(led_green,GPIO.LOW)
