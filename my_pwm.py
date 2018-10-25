61d3d9a8a47a37cc7f5975e29da7f4790cad9986
smysergio <smysergio@gmail.com>
2018-10-25 20:55:09 +0100

    pwm

--- my_pwm.py
+++ my_pwm.py
@@ -1,21 +1,45 @@
 import RPi.GPIO as GPIO     # Importing RPi library to use the GPIO pins
 from time import sleep  # Importing sleep from time library
-led_pin = 20            # Initializing the GPIO pin 21 for LED
+
+m1_enable = 13
+m2_enable = 17
+
+motors = [6,5,22,27]
+
 GPIO.setmode(GPIO.BCM)          # We are using the BCM pin numbering
-GPIO.setup(led_pin, GPIO.OUT)   # Declaring pin 21 as output pin
-pwm = GPIO.PWM(led_pin, 100)    # Created a PWM object
-pwm.start(0)                    # Started PWM at 0% duty cycle
-try:
-    while 1:                    # Loop will run forever
-        for x in range(100):    # This Loop will run 100 times
-            pwm.ChangeDutyCycle(x) # Change duty cycle
-            sleep(0.01)         # Delay of 10mS
-            
-        for x in range(100,0,-1): # Loop will run 100 times; 100 to 0
-            pwm.ChangeDutyCycle(x)
-            sleep(0.01)
-# If keyboard Interrupt (CTRL-C) is pressed
-except KeyboardInterrupt:
-    pass        # Go to next line
-pwm.stop()      # Stop the PWM
-GPIO.cleanup()  # Make all the output pins LOW
+
+GPIO.setup(m1_enable,GPIO.OUT)
+GPIO.setup(m2_enable,GPIO.OUT)
+GPIO.setup(motors[0],GPIO.OUT)
+GPIO.setup(motors[1],GPIO.OUT)
+GPIO.setup(motors[2],GPIO.OUT)
+GPIO.setup(motors[3],GPIO.OUT)
+
+GPIO.output(m1_enable,GPIO.HIGH)
+GPIO.output(m2_enable,GPIO.HIGH)
+
+GPIO.output(motors[0],GPIO.HIGH)
+GPIO.output(motors[1],GPIO.LOW)
+GPIO.output(motors[2],GPIO.HIGH)
+GPIO.output(motors[3],GPIO.LOW)
+
+time.sleep(5)
+
+GPIO.output(motors[0],GPIO.LOW)
+GPIO.output(motors[1],GPIO.LOW)
+GPIO.output(motors[2],GPIO.LOW)
+GPIO.output(motors[3],GPIO.LOW)
+
+time.sleep(5)
+
+GPIO.output(motors[0],GPIO.HIGH)
+GPIO.output(motors[1],GPIO.LOW)
+GPIO.output(motors[2],GPIO.HIGH)
+GPIO.output(motors[3],GPIO.LOW)
+
+time.sleep(5)
+
+GPIO.output(motors[0],GPIO.LOW)
+GPIO.output(motors[1],GPIO.LOW)
+GPIO.output(motors[2],GPIO.LOW)
+GPIO.output(motors[3],GPIO.LOW)
